a38aaf662b341f1171eec931cf97d1dc16462877
Matthew Moseby <matthew.moseby@gmail.com>
2019-01-12 20:50:12 -0600

    -Added a unity gitignore file. -Unpaired Unity's Collab features from project -Updated project's settings to be git friendly

---
+++ Group Project/Group Project/Library/Collab/Original/Assets/Scripts/Player/PlayerStatsTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatsTest : MonoBehaviour {
+
+    public int hp = 100;
+
+    public void TakeDamage(int amount)
+    {
+        hp -= amount;
+    }
+}
